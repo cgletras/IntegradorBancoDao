f9c641a9cda0de225fe1f3d64233e62a52216b21
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-08 17:27:45 -0300

    Produto JDBC feito com FindAllByUser, findById e insertProduct

--- src/model/dao/impl/ProdutoDaoJDBC.java
+++ src/model/dao/impl/ProdutoDaoJDBC.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -70,9 +71,85 @@ private Connection conn;
 	}
 
 	@Override
-	public List<Produto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Produto> findAllByUser(Usuario usuario) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT * FROM Produto "
+					+ "WHERE id_usuario = ?");
+			
+			st.setInt(1, usuario.getIdUsuario());
+			rs = st.executeQuery();
+			List<Produto> produtos = new ArrayList<Produto>();
+			
+			while (rs.next()) {
+				Produto obj = new Produto();
+				obj.setIdProduto(rs.getInt("id_produto"));
+				obj.setEditora(rs.getString("editora"));
+				obj.setTitulo(rs.getString("titulo"));
+				obj.setFormatoDoQuadrinho(rs.getString("formato_do_quadrinho"));
+				obj.setNumeroPaginas(rs.getInt("numero_paginas"));
+				obj.setPeso(rs.getInt("peso"));
+				obj.setCapaImagem(rs.getString("capa_imagem"));
+
+				EstadoProdutoDao estadoProdutoDao = DaoFactory.createEstadoProdutoDao();
+				EstadoProduto estadoProduto = estadoProdutoDao.findById(rs.getInt("id_estado_produto"));
+				obj.setEstado(estadoProduto);
+				
+				UsuarioDao usuarioDao = DaoFactory.createUsuarioDao();
+				Usuario user = usuarioDao.findById(rs.getInt("id_usuario"));
+				obj.setUsuario(user);
+				
+				produtos.add(obj);
+			}
+			return produtos;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
+	@Override
+	public void insertProduct(Produto obj) {
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"INSERT INTO Produto (editora, titulo, formato_do_quadrinho, numero_paginas, peso, capa_imagem, id_estado_produto, id_usuario) " + "VALUES " + "(?, ?, ?, ?, ?, ?, ?, ?)",
+					java.sql.Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, obj.getEditora());
+			st.setString(2, obj.getTitulo());
+			st.setString(3, obj.getFormatoDoQuadrinho());
+			st.setInt(4, obj.getNumeroPaginas());
+			st.setInt(5, obj.getPeso());
+			st.setString(6, obj.getCapaImagem());
+			st.setInt(7, obj.getEstado().getIdEstadoProduto());
+			st.setInt(8, obj.getUsuario().getIdUsuario());
+			
+			int rowsAffected = st.executeUpdate();
+		
+			if (rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				if (rs.next()) {
+					int id = rs.getInt(1);
+					obj.setIdProduto(id);
+				}
+				DB.closeResultSet(rs);
+			} 
+			else {
+				throw new DbException("Unexpected error! No rows affected!");
+			}
+		} 
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} 
+		finally {
+			DB.closeStatement(st);
+		}
+	}
 }
