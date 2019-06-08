1abc34c2a6c62f79eae26a45a3237ce2804be4ff
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-08 17:27:45 -0300

    Produto JDBC feito com FindAllByUser, findById e insertProduct

--- src/application/ProgramProduto.java
+++ src/application/ProgramProduto.java
@@ -1,24 +1,53 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.ProdutoDao;
+import model.entities.EstadoProduto;
 import model.entities.Produto;
+import model.entities.Usuario;
 
 public class ProgramProduto {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		int id_produto = 4;
-		Produto p  = carregarProdutoByID(id_produto);
-		System.out.println(p);
+		int id_usuario = 1;
 		
+	//	carregarProdutoByID(id_produto);
+		
+	// carregarProdutosPorUsuario(id_usuario);
+			
+	//	inserirProduto(id_usuario);
 	}
 
-	public static Produto carregarProdutoByID(int id) {
+	public static void inserirProduto(int id_usuario) {
 		ProdutoDao produtoDao = DaoFactory.createProdutoDao();
-		return produtoDao.findById(id);
+		Produto produto = new Produto();
+		produto.setEditora("Marvel");
+		produto.setTitulo("Greatest Vilains of the Fantastic Four");
+		produto.setFormatoDoQuadrinho("TPB");
+		produto.setNumeroPaginas(85);
+		produto.setPeso(150);
+		produto.setCapaImagem("capaImagem");
 		
+		EstadoProduto estado = new EstadoProduto();
+		estado.setIdEstadoProduto(1);
+		produto.setEstado(estado);
+		
+		produto.setUsuario(ProgramUsuario.carregaUsuario(id_usuario));
+		produtoDao.insertProduct(produto);
+	}
+
+	public static List<Produto> carregarProdutosPorUsuario(int id_usuario) {
+		ProdutoDao produtoDao = DaoFactory.createProdutoDao();
+		return produtoDao.findAllByUser(ProgramUsuario.carregaUsuario(id_usuario));
+	}
+
+	public static Produto carregarProdutoByID(int id_produto) {
+		ProdutoDao produtoDao = DaoFactory.createProdutoDao();
+		return produtoDao.findById(id_produto);
 	}
 
 }
--- src/model/dao/ProdutoDao.java
+++ src/model/dao/ProdutoDao.java
@@ -3,9 +3,11 @@ package model.dao;
 import java.util.List;
 
 import model.entities.Produto;
+import model.entities.Usuario;
 
 public interface ProdutoDao {
-
+	
+	void insertProduct(Produto produto);
 	Produto findById(Integer id);
-	List<Produto> findAll();
+	List<Produto> findAllByUser(Usuario usuario);
 }
