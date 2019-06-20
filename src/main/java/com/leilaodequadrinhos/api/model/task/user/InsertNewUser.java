b0d2208ddc646b5974adfecc09ac99fe963b7ee3
tiago <tiagomnunes@hotmail.com>
2019-06-20 10:17:59 -0300

    rename common tasks to basetask

--- src/main/java/com/leilaodequadrinhos/api/model/task/user/InsertNewUser.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/user/InsertNewUser.java
@@ -1,20 +1,15 @@
 package com.leilaodequadrinhos.api.model.task.user;
 
 import com.leilaodequadrinhos.api.model.dao.DAO;
-import com.leilaodequadrinhos.api.model.dao.UserDao;
 import com.leilaodequadrinhos.api.model.dao.impl.jdbc.UserDAO;
-
-import com.leilaodequadrinhos.api.model.entities.Produto;
 
 import com.leilaodequadrinhos.api.model.entities.User;
 import com.leilaodequadrinhos.api.model.task.Task;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
-public class InsertNewUser extends CommonUserTasks implements Task {
+public class InsertNewUser extends BaseUserTask implements Task {
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
