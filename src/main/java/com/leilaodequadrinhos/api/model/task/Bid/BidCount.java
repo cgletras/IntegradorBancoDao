fe46ac94b77c11734f479fca2500bc8882a79319
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-20 11:41:10 -0300

    Implementando o Contador de Lances BidCount, criei a package Bid no Tasks

---
+++ src/main/java/com/leilaodequadrinhos/api/model/task/Bid/BidCount.java
@@ -0,0 +1,13 @@
+package com.leilaodequadrinhos.api.model.task.Bid;
+
+import com.leilaodequadrinhos.api.model.task.Task;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class BidCount implements Task {
+    @Override
+    public Object execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return null;
+    }
+}
