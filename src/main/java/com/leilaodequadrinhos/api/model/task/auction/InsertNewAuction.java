2b07eb4f7615eb3dbbf1688a702c7ca9f2682670
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-20 03:01:56 -0300

    Atualizei Inserir leilao

--- src/main/java/com/leilaodequadrinhos/api/model/task/auction/InsertNewAuction.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/auction/InsertNewAuction.java
@@ -1,16 +1,19 @@
 package com.leilaodequadrinhos.api.model.task.auction;
 
-import com.leilaodequadrinhos.api.model.dao.*;
-import com.leilaodequadrinhos.api.model.dao.impl.jdbc.*;
-import com.leilaodequadrinhos.api.model.entities.EstadoLeilao;
+import com.leilaodequadrinhos.api.model.dao.EstadoLeilaoDao;
+import com.leilaodequadrinhos.api.model.dao.LeilaoDao;
+import com.leilaodequadrinhos.api.model.dao.ProdutoDao;
+import com.leilaodequadrinhos.api.model.dao.UserDao;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.EstadoLeilaoDAO;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.LeilaoDAO;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProdutoDAO;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.UserDAO;
 import com.leilaodequadrinhos.api.model.entities.Leilao;
-import com.leilaodequadrinhos.api.model.entities.User;
 import com.leilaodequadrinhos.api.model.task.Task;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class InsertNewAuction implements Task {
     @Override
