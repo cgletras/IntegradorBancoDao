a86ebaa63ebfc2afac9c98c61c50d9ca1aa68761
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-23 03:41:11 -0300

    RN ChangeProductStatus implementando no task e testado

--- src/main/java/com/leilaodequadrinhos/api/model/dao/ProductDao.java
+++ src/main/java/com/leilaodequadrinhos/api/model/dao/ProductDao.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface ProductDao extends DAO {
 
 	List<Product> findAllByUser(Long UserId);
-	void changeStatusProduct(Integer id, ProductStatus productStatus);
+	void changeStatusProduct(Long id, ProductStatus productStatus);
 }
--- src/main/java/com/leilaodequadrinhos/api/model/task/product/ChangeProductStatus.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/product/ChangeProductStatus.java
@@ -4,6 +4,7 @@ import com.leilaodequadrinhos.api.model.dao.ProductDao;
 import com.leilaodequadrinhos.api.model.dao.ProductStatusDao;
 import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProductDAO;
 import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProductStatusDAO;
+import com.leilaodequadrinhos.api.model.entities.Product;
 import com.leilaodequadrinhos.api.model.task.Task;
 
 import javax.servlet.http.HttpServletRequest;
@@ -15,9 +16,32 @@ public class ChangeProductStatus implements Task {
     public Object execute(HttpServletRequest request, HttpServletResponse response) {
         ProductDao productDao = new ProductDAO();
         ProductStatusDao productStatusDao = new ProductStatusDAO();
-        Integer productID = Integer.parseInt(request.getParameter("productID"));
+        Long productID = Long.parseLong(request.getParameter("productID"));
         Long productStateID = Long.parseLong(request.getParameter("productStateID"));
-        productDao.changeStatusProduct(productID, productStatusDao.findById(productStateID));
+        Product product = (Product) productDao.findById(productID);
+        switch (product.getProductStatus().getProductStatusID()){
+            case 1: if(productStateID == 2 ||productStateID == 3){
+                productDao.changeStatusProduct(productID, productStatusDao.findById(productStateID));
+                return "Successfully modified product status to "+productStatusDao.findById(productStateID).getStatus();
+            } else {
+                return "Cannot modify product status";
+            }
+            case 2: if(productStateID == 1) {
+                productDao.changeStatusProduct(productID, productStatusDao.findById(productStateID));
+                return "Successfully modified product status to "+productStatusDao.findById(productStateID).getStatus();
+            } else {
+                return "Cannot modify product status";
+            }
+            case 3: if(productStateID == 4){
+                productDao.changeStatusProduct(productID, productStatusDao.findById(productStateID));
+                return "Successfully modified product status to "+productStatusDao.findById(productStateID).getStatus();
+            } else if (productStateID == 1) {
+                productDao.changeStatusProduct(productID, productStatusDao.findById(productStateID));
+                return "Successfully modified product status to "+productStatusDao.findById(productStateID).getStatus();
+            } else {
+                return "Cannot modify product status";
+            }
+        }
         return "Successfully modified product status";
     }
 }
