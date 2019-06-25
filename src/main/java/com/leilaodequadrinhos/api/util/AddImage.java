4e926eb672cbb0ccba3bf2add5c4ccadcf1616e1
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-25 00:14:55 -0300

    Adicionei o endereço imagem/capas/ no set do endeço pra gerar o hashCode no endereço já

---
+++ src/main/java/com/leilaodequadrinhos/api/util/AddImage.java
@@ -0,0 +1,25 @@
+package com.leilaodequadrinhos.api.util;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
+
+public class AddImage {
+
+    private void addImage() {
+
+//        final JFileChooser chooser = new JFileChooser();
+//
+//        chooser.setFileFilter(new FileNameExtensionFilter("Arquivos de Imagem",
+//                "png", "jpeg", "jpg"));
+//        chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+//
+//        final int selection = chooser.showOpenDialog();
+//
+//        if (selection == JFileChooser.APPROVE_OPTION) {
+//            File selectedFile = chooser.getSelectedFile();
+//
+//            // resto do codigo tratando o arquivo selecionado
+//        }
+    }
+}
