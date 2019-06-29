7776ca2bf9e71315e5de4d5a104a151af2c336c6
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-29 16:06:24 -0300

    O estado do produto muda para em leilao ao criar um leilao

--- src/main/java/com/leilaodequadrinhos/api/model/task/auction/BaseAuctionTask.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/auction/BaseAuctionTask.java
@@ -1,6 +1,7 @@
 package com.leilaodequadrinhos.api.model.task.auction;
 
 import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProductDAO;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProductStatusDAO;
 import com.leilaodequadrinhos.api.model.dao.impl.jdbc.UserDAO;
 import com.leilaodequadrinhos.api.model.entities.Auction;
 import com.leilaodequadrinhos.api.model.entities.User;
@@ -11,9 +12,12 @@ import java.text.SimpleDateFormat;
 
 abstract class BaseAuctionTask {
 
+    private static final long INAUCTION = 3;
+
     protected Auction buildAuction(HttpServletRequest request, ProductDAO productDAO, UserDAO userDao) throws ParseException {
         Long userID = Long.valueOf((((User) request.getSession().getAttribute("user")).getUserID()));
         Long productID = Long.parseLong(request.getParameter("productID"));
+        ProductStatusDAO productStatusDAO = new ProductStatusDAO();
         Auction auction = new Auction();
         auction.setDuration(Integer.parseInt(request.getParameter("duration")));
         String format = "dd/MM/yyyy";
@@ -22,6 +26,7 @@ abstract class BaseAuctionTask {
         auction.setCurrentValue(auction.getInitialValue());
         auction.setDefaultBid(Float.parseFloat(request.getParameter("baseBid")));
         auction.setProduct(productDAO.findById(productID));
+        productDAO.changeStatusProduct(productID, productStatusDAO.findById(INAUCTION));
         auction.setUser(userDao.findById(userID));
         return auction;
     }
