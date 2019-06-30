d0e51300f4ea028b7b7b39bc763cf9716583f962
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-06-30 01:22:03 -0300

    Implementei a mudança de estado no produto quando DELETA um leilao o produto volta ao estado ativo. Quando cria um leilao o produto muda o estado para em leilao

--- src/main/java/com/leilaodequadrinhos/api/model/task/auction/DeleteAuctionByID.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/auction/DeleteAuctionByID.java
@@ -1,7 +1,14 @@
 package com.leilaodequadrinhos.api.model.task.auction;
 
 import com.leilaodequadrinhos.api.model.dao.AuctionDao;
+import com.leilaodequadrinhos.api.model.dao.ProductDao;
+import com.leilaodequadrinhos.api.model.dao.ProductStatusDao;
 import com.leilaodequadrinhos.api.model.dao.impl.jdbc.AuctionDAO;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProductDAO;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProductStatusDAO;
+import com.leilaodequadrinhos.api.model.entities.Auction;
+import com.leilaodequadrinhos.api.model.entities.Product;
+import com.leilaodequadrinhos.api.model.entities.ProductStatus;
 import com.leilaodequadrinhos.api.model.task.Task;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +20,14 @@ public class DeleteAuctionByID implements Task {
     public Object execute(HttpServletRequest request, HttpServletResponse response) {
         Long auctionID = Long.parseLong(request.getParameter("auctionID"));
         AuctionDao auctionDao = new AuctionDAO();
+        ProductDao productDao = new ProductDAO();
+
+        Auction auction = (Auction) auctionDao.findById(auctionID);
+        Product product = auction.getProduct();
+        ProductStatus productStatus = product.getProductStatus();
+        productStatus.setProductStatusID(1);
+        productDao.changeStatusProduct((long) product.getProductID(), productStatus);
+
         auctionDao.deleteById(auctionID);
         return "This auction has been deleted";
     }
