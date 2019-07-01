37777ac8c18850671621a6f0d78e9c8472c56446
Carlos Gustavo de Lacerda Stein <cgletras@gmail.com>
2019-07-01 20:57:48 -0300

    Update do auction JDBC para optimizar a busca no banco

--- src/main/java/com/leilaodequadrinhos/api/model/task/product/BaseProductTask.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/product/BaseProductTask.java
@@ -18,7 +18,7 @@ abstract class BaseProductTask {
         product.setPagesNumber(pages);
         int weight = Integer.parseInt(request.getParameter("weight"));
         product.setWeight(weight);
-        product.setCoverImage("img/capas/"+HashGenerator.hashGenerator()+".jpeg");
+        product.setCoverImage("https://leilaodequadrinhos.com/public/img/capas/"+HashGenerator.hashGenerator()+".jpeg");
 
         Long userID = Long.valueOf((((User) request.getSession().getAttribute("user")).getUserID()));
         product.setUser((User) userDao.findById(userID));
