cebcfe7eff5160e68d07d10c92846a2ca0ac473d
Tiago <tiagomnunes@hotmail.com>
2019-07-03 14:40:08 -0300

    replace variable list observable

--- src/main/java/com/leilaodequadrinhos/api/observer/interfaces/IObservableTask.java
+++ src/main/java/com/leilaodequadrinhos/api/observer/interfaces/IObservableTask.java
@@ -2,8 +2,12 @@ package com.leilaodequadrinhos.api.observer.interfaces;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public interface IObservableTask {
+
+    List<IObserverSendEmail> observerList = new ArrayList<>();
 
     void add(IObserverSendEmail observer);
     void remove(IObserverSendEmail observer);
