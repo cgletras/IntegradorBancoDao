f079217ad13c7fd33fb5f42b6b5208a0476ada1b
Eduardo Augusto <eduardoaugusto2016880@gmail.com>
2019-07-03 20:07:10 -0300

    Retirando necessidade de passar o id do usuario na Task FindBidsByUser

--- src/main/java/com/leilaodequadrinhos/api/model/task/bid/FindBidsByUser.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/bid/FindBidsByUser.java
@@ -3,6 +3,7 @@ package com.leilaodequadrinhos.api.model.task.bid;
 import com.leilaodequadrinhos.api.model.dao.BidDao;
 import com.leilaodequadrinhos.api.model.dao.impl.jdbc.BidDAO;
 import com.leilaodequadrinhos.api.model.entities.Bid;
+import com.leilaodequadrinhos.api.model.entities.User;
 import com.leilaodequadrinhos.api.model.task.Task;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,7 +15,7 @@ public class FindBidsByUser implements Task {
     @Override
     public Object execute(HttpServletRequest request, HttpServletResponse response) {
 
-        Long userID = Long.parseLong(request.getParameter("userID"));
+        Long userID = Long.valueOf((((User) request.getSession().getAttribute("user")).getUserID()));
         BidDao bidDao = new BidDAO();
         List<Bid> bids = bidDao.findBidsByUser(userID);
         request.setAttribute("bids", bids);
