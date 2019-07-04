d6aca0fec0231bbcbb3716d661f938b465a3210e
Tiago <tiagomnunes@hotmail.com>
2019-07-03 21:01:00 -0300

    changes on insert bid task

--- src/main/java/com/leilaodequadrinhos/api/model/task/bid/InsertBid.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/bid/InsertBid.java
@@ -51,35 +51,58 @@ public class InsertBid implements Task, IObservableTask {
         switch (auction.getAuctionStatus().getStatus()) {
             case "ATIVO":
                 bidDao.insert(bid);
-                CallSendEmail(this);
+                CallSendEmail(this,auctionID);
                 return "Bid successfully entered";
             default:
                 return "Bids could only be placed on active auctions";
         }
     }
 
-    private void CallSendEmail(InsertBid insertBid) {
+    private void CallSendEmail(InsertBid insertBid, Long auctionID) {
         insertBid.observerList.add(request -> {
-            Email from = new Email(request.getParameter("emailFrom"));
-            String subject = request.getParameter("titleEmail");
-            Email to = new Email(request.getParameter("emailTo"));
-            Content content = new Content("text/plain", request.getParameter("content"));
-            Mail mail = new Mail(from, subject, to, content);
+            BidDao bidDao = new BidDAO();
+            List<Bid> bids = bidDao.findBidsByAuction(auctionID);
+            for(Bid bid : bids){
+                Email from = new Email("notification@leilaoquadrinhos.com");
+                String subject = "Notificação de Lance no Leilão do Quadrinho "+ bid.getAuction().getProduct().getTitle();
+                Email to = new Email(bid.getUser().getEmail());
+                Content content = new Content("text/html", "<p><strong><span style=\"font-size:16px\"><span style=\"font-family:comic sans ms,cursive\">Ol&aacute; "+ bid.getUser().getName() +",</span></span></strong></p>\n" +
+                        "\n" +
+                        "<p>&nbsp;</p>\n" +
+                        "\n" +
+                        "<p><span style=\"font-size:16px\"><span style=\"font-family:comic sans ms,cursive\">Informamos que outro usu&aacute;rio cobriu seu&nbsp;lance no quadrinho "+ bid.getAuction().getProduct().getTitle() +".&nbsp;</span></span></p>\n" +
+                        "\n" +
+                        "<p><span style=\"font-size:16px\"><span style=\"font-family:comic sans ms,cursive\">Para continuar na disputa acesse nossa p&aacute;gina:&nbsp;</span></span></p>\n" +
+                        "\n" +
+                        "<p><span style=\"font-size:16px\"><span style=\"font-family:comic sans ms,cursive\"><a href=\"https://leilaodequadrinhos.com/leilao/"+ auctionID +"\">https://leilaodequadrinhos.com/leilao/"+ auctionID +"</a></span></span></p>\n" +
+                        "\n" +
+                        "<p>&nbsp;</p>\n" +
+                        "\n" +
+                        "<p><span style=\"font-size:16px\"><span style=\"font-family:comic sans ms,cursive\">Agradecemos sua participa&ccedil;&atilde;o!</span></span></p>\n" +
+                        "\n" +
+                        "<p>&nbsp;</p>\n" +
+                        "\n" +
+                        "<p><span style=\"font-size:16px\"><span style=\"font-family:comic sans ms,cursive\">Atenciosamente,</span></span></p>\n" +
+                        "\n" +
+                        "<p><strong><font face=\"comic sans ms, cursive\"><span style=\"font-size:16px\">Equipe Leil&atilde;o de Quadrinhos</span></font></strong></p>\n");
+                Mail mail = new Mail(from, subject, to, content);
 
-            SendGrid sg = new SendGrid(API_KEY);
-            Request requestSG = new Request();
-            try {
-                requestSG.setMethod(Method.POST);
-                requestSG.setEndpoint("mail/send");
-                requestSG.setBody(mail.build());
-                Response responseSG = sg.api(requestSG);
-                System.out.println(responseSG.getStatusCode());
-                System.out.println(responseSG.getBody());
-                System.out.println(responseSG.getHeaders());
-            } catch (IOException ex) {
-                throw ex;
+                SendGrid sg = new SendGrid(API_KEY);
+                Request requestSG = new Request();
+                try {
+                    requestSG.setMethod(Method.POST);
+                    requestSG.setEndpoint("mail/send");
+                    requestSG.setBody(mail.build());
+                    Response responseSG = sg.api(requestSG);
+                    System.out.println(responseSG.getStatusCode());
+                    System.out.println(responseSG.getBody());
+                    System.out.println(responseSG.getHeaders());
+                } catch (IOException ex) {
+                    throw ex;
+                }
             }
-            return "E-mail enviado com sucesso";
+
+            return "Notificações por email enviadas com sucesso";
         });
     }
 
