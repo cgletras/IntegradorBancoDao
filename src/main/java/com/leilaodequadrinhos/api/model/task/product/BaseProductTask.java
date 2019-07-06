edce6c1795805986942623467ea58cf2d529243f
Rafael Sotero Rocha <29471402+soterocra@users.noreply.github.com>
2019-07-06 18:35:37 -0300

    Adicionado correções ao update

--- src/main/java/com/leilaodequadrinhos/api/model/task/product/BaseProductTask.java
+++ src/main/java/com/leilaodequadrinhos/api/model/task/product/BaseProductTask.java
@@ -1,6 +1,8 @@
 package com.leilaodequadrinhos.api.model.task.product;
 
+import com.leilaodequadrinhos.api.model.dao.ProductDao;
 import com.leilaodequadrinhos.api.model.dao.UserDao;
+import com.leilaodequadrinhos.api.model.dao.impl.jdbc.ProductDAO;
 import com.leilaodequadrinhos.api.model.entities.Product;
 import com.leilaodequadrinhos.api.model.entities.User;
 import com.leilaodequadrinhos.api.util.HashGenerator;
@@ -20,7 +22,15 @@ abstract class BaseProductTask {
         product.setPagesNumber(pages);
         int weight = Integer.parseInt(request.getParameter("weight"));
         product.setWeight(weight);
-        product.setCoverImage(new ReceiveProductImage().execute(request));
+
+        if (request.getParameter("updateImage") != null && (Boolean.parseBoolean(request.getParameter("updateImage")))) {
+            product.setCoverImage(new ReceiveProductImage().execute(request));
+        } else {
+            product.setCoverImage(
+                new ProductDAO().findById(Long.valueOf(request.getParameter("productID"))).getCoverImage()
+            );
+        }
+
         Long userID = Long.valueOf((((User) request.getSession().getAttribute("user")).getUserID()));
         product.setUser((User) userDao.findById(userID));
         return product;
